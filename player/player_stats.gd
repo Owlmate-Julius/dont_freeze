e9403f8cb52b4a73c8e232dd8f2af8530638f4bb
Muraylo, Yuliy <yuliy.muraylo@stud.th-luebeck.de>
2024-02-19 16:54:19 +0100

    ...

---
+++ player/player_stats.gd
@@ -0,0 +1,9 @@
+class_name PlayerStats extends Resource
+
+@export var health : float
+@export var hunger : float
+
+@export var chop_strength : float
+@export var strength : float
+@export var deffence : float
+@export var luck : float
